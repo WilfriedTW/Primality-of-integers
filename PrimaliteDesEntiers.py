c5f44e6ece0a21624909bcf6e362ac2cea5e5d15
Wilfried TW <wilfriedtelli092@gmail.com>
2023-05-19 18:16:05 +0000

    Check if your number is prime or not

---
+++ PrimaliteDesEntiers.py
@@ -0,0 +1,27 @@
+"""This algorithm is used to check the primality of integers from 0 to 50000.
+Beyond this interval, it gives erroneous results."""
+
+
+import math
+
+
+def primalite(n):
+    if n < 2:
+        print(n, "is not prime")
+        return
+
+    if n == 2:
+        print(n, "is prime")
+        return
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            print(n, "is not prime")
+            return
+
+    print(n, "is prime")
+
+
+number = int(input("Enter your number : "))
+
+primalite(number)
